780d5d1808042fcf9f03dfa732ee01c7d6a8fdd4
Расим Шаймарданов <shaimardanoff92@yandex.ru>
2023-12-18 15:04:48 +0300

    Запуск №2

--- pep_parse/settings.py
+++ pep_parse/settings.py
@@ -21,7 +21,7 @@ REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
 FEEDS = {
-    f'{BASE_DIR}/{RESULTS}/pep_%(time)s.csv': {
+    f'{RESULTS}/pep_%(time)s.csv': {
         'format': 'csv',
         'fields': ['number', 'name', 'status'],
         'overwrite': True
